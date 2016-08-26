187cbae945f65f65b6bb313ac247e918bee65a18
Allie Q <aquintan@thoughtworks.com>
2016-08-26 11:34:00 -0400

    add simple test; working with ReSharper test runner in VS15

---
+++ Assets/Scripts/test/ASimpleClassTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.test
+{
+    using System;
+    using NUnit.Framework;
+    using Assets.Scripts.src;
+
+    [TestFixture]
+    class ASimpleClassTest
+    {
+        private ASimpleClass aSimpleClass;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            aSimpleClass = new ASimpleClass();
+        }
+
+        [Test]
+        public void AddsTwo()
+        {
+            Assert.AreEqual(4, aSimpleClass.addTwo(2));
+        }
+    }
+}
