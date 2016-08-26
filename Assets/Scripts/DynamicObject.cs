3a3498ec694242782dcd791479ede61dccd2061c
Allie Q <aquintano22@gmail.com>
2016-08-26 11:11:05 -0400

    disables buggy pausing for now

--- Assets/Scripts/DynamicObject.cs
+++ Assets/Scripts/DynamicObject.cs
@@ -112,7 +112,7 @@ public class DynamicObject : MonoBehaviour
 
     void PauseMovement()
     {
-        enableMovement = !enableMovement;
+        //enableMovement = !enableMovement;
     }
 
     void PlayAnimation()
